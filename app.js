8d9e67bbeb913113e22ce52d6f12a22b3c2bfab8
Chris Latin <heatherlatin@gmail.com>
2019-11-02 16:21:45 -0700

    combined server.js and app.js

--- app.js
+++ app.js
@@ -7,17 +7,30 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+//Setting up our PORT
+var PORT = process.env.PORT || 8080;
+
+
+// Requiring our models for syncing
+var db = require("./models");
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(logger('dev'));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+
+
+app.use(logger('dev'));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Static directory
+app.use(express.static("public"));
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -37,5 +50,18 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT http://localhost:" + PORT);
+  });
+});
+
 
 module.exports = app;
