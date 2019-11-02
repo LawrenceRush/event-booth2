7a6afa5fcd6102d13fdfc0596039648efa5b259e
Lawrence <l.rush7@gmail.com>
2019-11-02 16:43:55 -0700

    Have new intergrated server

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+
 
 //Setting up our PORT
 var PORT = process.env.PORT || 8080;
@@ -33,7 +33,7 @@ app.use(express.static("public"));
 
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
