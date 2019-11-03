8a4b6581a2c8d0cfefa45723e5051927d22d5ca2
Chris Latin <heatherlatin@gmail.com>
2019-11-02 17:01:57 -0700

    not working, committing to pull Lawrence's

--- app.js
+++ app.js
@@ -31,27 +31,30 @@ app.use(cookieParser());
 app.use(express.static("public"));
 
 
-// app.use('/', indexRouter);
+app.use('/', indexRouter);
 
 // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   next(createError(404));
-// });
+app.use(function(req, res, next) {
+  next(createError(404));
+});
 
-// // error handler
-// app.use(function(err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
 
 // Routes
 // =============================================================
-require("./routes/api-routes.js")(app);
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
+// require("./routes/index.js")(app);
+
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
